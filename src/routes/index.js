69f2776f18402ae2aedb1d12cb35042401c84ee5
NHau66 <nguyenhau65466@gmail.com>
2022-11-22 14:06:30 +0700

    paging

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,14 +5,21 @@ import SourceCodeManagement from '~/pages/SourceCodeManagement/SourceCodeManagem
 import UserVNPManagement from '~/pages/UserVNPManagement/UserVNPManagement';
 import MenuManagement from '~/pages/MenuManagement/MenuManagement';
 import HomeAdd from '~/pages/Home/HomeAdd';
+import HomeEdit from '~/pages/Home/HomeEdit';
 
 const publicRoutes = [
     { path: '/usermanagement/user', component: Home },
     { path: '/usermanagement/user/add', component: HomeAdd },
+    { path: '/usermanagement/user/edit', component: HomeEdit },
+
     { path: '/usermanagement/group', component: GroupRule },
+
     { path: '/usermanagement/menu', component: MenuManagement },
+
     { path: '/servermanagement/regionserver', component: ServerManagement },
+
     { path: '/uservnpmanagement/user', component: UserVNPManagement },
+
     { path: '/sourcecodemanagement/module', component: SourceCodeManagement },
 ];
 
